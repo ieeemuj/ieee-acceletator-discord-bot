ce2ad8da99e165ddc44f1d941a97d436e9c198f7
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-05-30 05:05:29 +0530

    prefix fetch on message events

--- src/events/message/MessageEvent.js
+++ src/events/message/MessageEvent.js
@@ -1,21 +1,28 @@
-const BaseEvent = require('../../utils/structures/BaseEvent');
+const BaseEvent = require("../../utils/structures/BaseEvent");
+const db = require("../../db/firestore");
+let prefix;
 
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
-    super('message');
+    super("message");
   }
-  
+
   async run(client, message) {
+    let result = await db.collection("guilds").doc(message.guild.id).get();
+    if (result.exists) {
+      prefix = result.data().prefix;
+    }
+
     if (message.author.bot) return;
-    if (message.content.startsWith(client.prefix)) {
+    if (message.content.startsWith(prefix)) {
       const [cmdName, ...cmdArgs] = message.content
-      .slice(client.prefix.length)
-      .trim()
-      .split(/\s+/);
+        .slice(prefix.length)
+        .trim()
+        .split(/\s+/);
       const command = client.commands.get(cmdName);
       if (command) {
         command.run(client, message, cmdArgs);
       }
     }
   }
-}
+};
