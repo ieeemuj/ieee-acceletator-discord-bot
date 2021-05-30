842f342c8a90175caf3323974153276369b88a22
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-05-30 23:10:55 +0530

    on guild join, enter into firebase the guild info

--- src/events/GuildCreateEvent.js
+++ src/events/GuildCreateEvent.js
@@ -8,19 +8,41 @@ module.exports = class GuildCreateEvent extends BaseEvent {
   }
 
   async run(client, guild) {
+    const mentorRole = "mentor";
+    const role = guild.roles.cache.find((r) => r.name === mentorRole);
+    const mentorRoleID = role.id;
+    const mentorsCollection = role.members;
+    let mentorsDetails = [];
+
+    for (const [key, value] of mentorsCollection) {
+      mentorsDetails.push({
+        id: key,
+        name: value.user.username,
+      });
+    }
+    console.log(role);
+    console.log(mentorsCollection);
+
     db.collection("guilds")
       .doc(guild.id)
       .set({
         guildID: guild.id,
         guildName: guild.name,
         prefix: "./",
-        mentorID: "847916638279827506",
+        mentorRoleID: mentorRoleID,
+        mentors: mentorsDetails,
+        // have to be filled in the setup page
         voiceChannels: [
           {
             name: "accelerator-vc-1",
             id: "847916638279827506",
           },
+          {
+            name: "accelerator-vc-2",
+            id: "848361374904221746",
+          },
         ],
+        setup: true,
       });
   }
 };
