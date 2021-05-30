72ffd8342f8391f72cdcc11cdf4d4f98a157334b
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-05-30 23:11:30 +0530

    for messages check prefix set using firebase

--- src/events/message/MessageEvent.js
+++ src/events/message/MessageEvent.js
@@ -1,22 +1,32 @@
 const BaseEvent = require("../../utils/structures/BaseEvent");
 const db = require("../../db/firestore");
-let prefix;
 
+let clientConfig = {
+  prefix: "",
+  mentorID: "",
+};
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
     super("message");
   }
 
   async run(client, message) {
+    if (message.author.bot) return;
+
     let result = await db.collection("guilds").doc(message.guild.id).get();
     if (result.exists) {
-      prefix = result.data().prefix;
+      clientConfig.prefix = result.data().prefix;
+      clientConfig.mentorRoleID = result.data().mentorRoleID;
     }
 
-    if (message.author.bot) return;
-    if (message.content.startsWith(prefix)) {
+    if (!message.member._roles.includes(clientConfig.mentorRoleID)) {
+      message.channel.send("Only Mentors can manage sessions");
+      return;
+    }
+
+    if (message.content.startsWith(clientConfig.prefix)) {
       const [cmdName, ...cmdArgs] = message.content
-        .slice(prefix.length)
+        .slice(clientConfig.prefix.length)
         .trim()
         .split(/\s+/);
       const command = client.commands.get(cmdName);
